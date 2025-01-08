4638486eb31dfe3a3dd4eae11b1461ee0e804058
michaelw123 <michael.xiaoqun.wang@gmail.com>
2025-01-08 09:14:41 -0500

    bug fixes

--- src/test/scala/ThermometerContinuationTest.scala
+++ src/test/scala/ThermometerContinuationTest.scala
@@ -1,8 +1,9 @@
 import Thermometer.{reset, shift}
-given state: ContinuationState[Int] = ContinuationState[Int]
+given IntState: ContinuationState[Int] = ContinuationState[Int]
+given stringState: ContinuationState[String] = ContinuationState[String]
 
 
-def test:Unit = {
+def test1:Unit = {
   // 2*5 + 1 = 11
   val x = reset {
     2 * shift { (k: Int => Int) => 1 + k(5)
@@ -11,7 +12,7 @@ def test:Unit = {
   println(x)
 }
 
-def test1:Unit = {
+def test2:Unit = {
   // (1+1) * (1+2) * (1+3) = 24
   val x = reset {
     1 + shift{
@@ -21,7 +22,7 @@ def test1:Unit = {
   println(x)
 }
 
-def test2: Unit = {
+def test3: Unit = {
   // 1 + (3*(2+5)) = 22
   val x = 1 + reset {
     2 + shift {
@@ -30,28 +31,24 @@ def test2: Unit = {
   }
   println(x)
 }
-
-def test3: Unit = {
-  // 1+ 3*(2+10)=37
-  val x = 1 + reset {
-    2 + shift {
-      (k: Int => Int) => 3 * shift { (l: Int => Int) => l(k(10)) }
-    }
-  }
-  println(x)
-}
-
-def test4: Unit = {
-  // 2*2*4 = 16
+def test4: Unit={
+  //2 + (((1 + (2*5)) +6)  = 19
   val x = reset {
-    2 * shift {
-      (k: Int => Int) => k(k(4))
-    }
+    2 *  shift{(k: Int => Int) => 1 +k(5)}  + shift{(k: Int => Int) => 2 +k(6)}
+  }
+
+  println(x)
+
+}
+
+def test5:Unit = {
+  // 1+2*2*5 = 21
+  val x = reset {
+    2 * shift { (k: Int => Int) => 1 + k(k(5)) }
   }
   println(x)
 }
-
-def test5: Unit = {
+def test6: Unit = {
   // (2*3) + (2*5) + (1+4) = 21
   val x = reset {
     1 + shift {
@@ -66,60 +63,29 @@ def test5: Unit = {
   }
   println(x)
 }
-def test6: Unit = {
-  //1 + 2*5 = 11
-  val x = reset {
-    2 * shift { (k: Int => Int) =>
-      1 + k(5)
-      }
+def test7={
+  //2+3=5
+  val x = reset{
+    1 + shift{(k: Int => Int) => 2+3}
   }
   println(x)
 }
 
-def test7: Unit = {
-  //(1+4) +(2+3) +5 = 15
-  val x = reset {
-    1 + shift{
-      (k: Int =>Int) =>
-        reset {
-          2 + shift{ (l: Int => Int) =>
-            5 + l(3) + k(4)
-            }
-        }
-    }
-  }
-  println(x)
-}
-def test8: Unit = {
-  // 1 + (2*5) * (2*6) = 121
-  val x = reset {
-    2 * shift { (k: Int => Int) =>
-      1+ k(5) * k(6)
-      }
+def test8={
+  //b+("a"+"c") = "bac"
+  val x = reset{
+    "a" + shift{(k: String => String) => "b" + k("c")}
   }
   println(x)
 }
 
-def test9: Unit={
-  //2 + (((1 + (2*5)) +6)  = 19
-  val x = reset {
-    2 *  shift{(k: Int => Int) => 1 +k(5)}  + shift{(k: Int => Int) => 2 +k(6)}
-  }
-
-  println(x)
-
-}
 @main def tests:Unit =
-  test //11
-  /*
-  test1 //24
-  test2 //22
-  test3 //37
-  test4 //16
-  test5 //21
-  test6  //11
-  test7  //15
-  test8  //121
-  test9  //19
-  */
-   
+  test1 //11
+  test2 //24
+  test3 //22
+  test4  //19
+  test5  //21
+  test6 //21
+  test7 //5
+  test8  //bac
+
